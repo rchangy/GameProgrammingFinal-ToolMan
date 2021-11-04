3476aab615341d88348eee9c9e6408da73a414b5
han-chihchang <han-chihchang@Han-Chihde-MBP.(none)>
2021-11-04 17:12:14 +0800

    first push

---
+++ ToolMan/Assets/Script/BearController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(Animator))]
+public class BearController : MonoBehaviour
+{
+
+    [SerializeField] private CharacterController characterController;
+    private Animator animator;
+    public int cc = 0;
+
+    /*[SerializeField] private float movementSpeed, rotationSpeed, gravity;
+
+    private Vector3 movementDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        Debug.Log("start: " + transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement
+        Vector3 inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
+        characterController.Move(inputMovement * Time.deltaTime);
+
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
+
+        // gravity (position.y will change according to the height of ground)
+        movementDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(movementDirection * Time.deltaTime);
+
+        animator.SetBool("isWalking", Input.GetAxisRaw("Vertical") != 0);
+    }*/
+
+    public Transform mainCameraTrans;
+
+    [SerializeField] private float speed = 5;
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (movement.sqrMagnitude > 0.01f)
+        {
+            // Facing angle (smoothed)
+            float movementAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + mainCameraTrans.eulerAngles.y;
+            float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, movementAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, smoothedAngle, 0);
+
+            // Move
+            Vector3 adjustedMovement = Quaternion.Euler(0, movementAngle, 0) * Vector3.forward; // Relative to mainCameraTrans
+            Debug.Log(adjustedMovement.magnitude);
+            characterController.Move(adjustedMovement * speed * Time.deltaTime);
+
+        }
+
+    }
+}
---
+++ ToolMan/Assets/Script/Enemy.cs
@@ -0,0 +1,197 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected NavMeshAgent EnemyAgent;
+
+    [SerializeField] protected Transform[] Players;
+
+    [SerializeField] protected LayerMask GroundMask, PlayerMask;
+
+    [SerializeField] protected string Name;
+
+    [SerializeField] protected HealthBar healthBar;
+
+    [SerializeField] protected int TotalHP;
+    private int _hp;
+    public int HP
+    {
+        get => _hp;
+        set
+        {
+            _hp = Mathf.Max(0, value);
+            healthBar.SetHealth(_hp);
+            if (_hp == 0) Die();
+        }
+    }
+    
+
+    // Patrol
+    protected Vector3 walkPoint;
+    protected bool walkPointSet;
+    [SerializeField] protected float walkPointRange;
+
+
+    // chase player
+    [SerializeField] protected float SightRange;
+    protected bool PlayerInSightRange;
+
+
+    // attack
+    [SerializeField] protected float InitAttackInterval;
+    private float _attackInterval;
+    public float AttackInterval
+    {
+        get => _attackInterval;
+        set
+        {
+            _attackInterval = Mathf.Max(0, value);
+        }
+    }
+
+    [SerializeField] protected int InitAttackRange;
+    private int _attackRange;
+    public int AttackRange
+    {
+        get => _attackRange;
+        set
+        {
+            _attackRange = value;
+        }
+    }
+
+    protected bool AlreadyAttacked;
+    protected bool PlayerInAttackRange;
+
+    // state & attributes (suppressed by what)
+    // health bar https://www.youtube.com/watch?v=37hEX3Lrc0A
+
+    protected virtual void Start()
+    {
+        EnemyAgent = GetComponent<NavMeshAgent>();
+
+        // get players
+        GameObject[] PlayerGameObjects = GameObject.FindGameObjectsWithTag("Player");
+        int playerNum = PlayerGameObjects.Length;
+
+        Players = new Transform[playerNum];
+        for (int i = 0; i < playerNum; i++)
+        {
+            Players[i] = PlayerGameObjects[i].transform;
+        }
+
+        // initialize attributes
+        healthBar.SetMaxHealth(TotalHP);
+        HP = TotalHP;
+        AttackInterval = InitAttackInterval;
+        AttackRange = InitAttackRange;
+    }
+
+    protected virtual void Update()
+    {
+        // check sight and attack range
+        PlayerInSightRange = Physics.CheckSphere(transform.position, SightRange, PlayerMask);
+        PlayerInAttackRange = Physics.CheckSphere(transform.position, AttackRange, PlayerMask);
+
+        if (!PlayerInSightRange && !PlayerInAttackRange) Patrol();
+        if (PlayerInSightRange && !PlayerInAttackRange) ChasePlayer();
+        if (PlayerInSightRange && PlayerInAttackRange) Attack();
+    }
+
+    protected virtual void Patrol()
+    {
+        Debug.Log("Patrol Mode");
+        if (!walkPointSet) SearchWalkPoint();
+        if (walkPointSet)
+            EnemyAgent.SetDestination(walkPoint);
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
+    }
+
+    protected virtual void Idle()
+    {
+        EnemyAgent.SetDestination(transform.position);
+    }
+
+    protected virtual void ChasePlayer()
+    {
+        Debug.Log("Chase Mode");
+        // compare two players position and chase the closest
+        Transform closestPlayer = GetClosestplayer();
+        EnemyAgent.SetDestination(closestPlayer.position);
+    }
+
+    protected virtual void Attack()
+    {
+        Debug.Log("Attack Mode");
+        EnemyAgent.SetDestination(transform.position);
+
+        if (!AlreadyAttacked)
+        {
+            // attack
+            // TODO
+            Debug.LogFormat("[{0}] Enemy Attacked", Name);
+            AlreadyAttacked = true;
+            Invoke(nameof(ResetAttack), AttackInterval);
+        }
+
+    }
+
+    protected virtual void ResetAttack()
+    {
+        AlreadyAttacked = false;
+    }
+
+    public virtual void BeAttacked()
+    {
+
+    }
+
+    protected void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, GroundMask)) walkPointSet = true;
+    }
+
+    protected Transform GetClosestplayer()
+    {
+        Transform target = null;
+        float minDist = float.MaxValue;
+        foreach(Transform player in Players)
+        {
+            float dist = Vector3.Distance(transform.position, player.position);
+            if(dist < minDist)
+            {
+                target = player;
+                minDist = dist;
+            }
+        }
+        return target;
+    }
+
+    protected virtual void Damaged(int dmg)
+    {
+        this.HP -= dmg;
+    }
+    protected virtual void Healed(int heal)
+    {
+        this.HP += heal;
+    }
+
+    protected virtual void Die()
+    {
+        Debug.LogFormat("[{0}] Enemy Dies", Name);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, AttackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, SightRange);
+    }
+}
