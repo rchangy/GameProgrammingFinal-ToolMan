5bce2073dd4b8e95ab73346f5ca3bc7a7f9562e1
lyanling <smile8www@gmail.com>
2021-11-06 17:20:25 +0800

    add Animator & ToolMan.cs

---
+++ ToolMan/Assets/Script/ToolMan.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(Animator))]
+public class ToolMan : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void toPickaxe()
+    {
+        animator.SetBool("isTool", true);
+        animator.SetBool("isPickaxe", true);
+    }
+
+    protected void toBoomerang()
+    {
+        animator.SetBool("isTool", true);
+        animator.SetBool("isBoomerang", true);
+    }
+
+    protected void toSword()
+    {
+        animator.SetBool("isTool", true);
+        animator.SetBool("isSword", true);
+    }
+
+    protected void toFlashBomb()
+    {
+        animator.SetBool("isTool", true);
+        animator.SetBool("isFlashBomb", true);
+    }
+
+    protected void toShield()
+    {
+        animator.SetBool("isTool", true);
+        animator.SetBool("isShield", true);
+    }
+
+    protected void toMan()
+    {
+        animator.SetBool("isTool", false);
+        animator.SetBool("isShield", false);
+        animator.SetBool("isFlashBomb", false);
+        animator.SetBool("isSword", false);
+        animator.SetBool("isBoomerang", false);
+        animator.SetBool("isPickaxe", false);
+    }
+}
