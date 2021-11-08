d4a2800d5470885e953ac6f2df28d193496fac3e
han-chihchang <han-chihchang@Han-Chihde-MBP.(none)>
2021-11-08 20:41:28 +0800

    skill module

---
+++ ToolMan/Assets/Script/Combat/ShootingSkill.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+public abstract class ShootingSkill : Skill
+{ 
+    public Transform ShootingPoint;
+    public float AttackDelay;
+    public GameObject BulletPrefab;
+    public float ShootingForce;
+
+}
---
+++ ToolMan/Assets/Script/Combat/Skill.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * defines an attack
+*/
+[System.Serializable]
+public abstract class Skill
+{
+    public int a;
+    public abstract void Attack(int Atk);
+    protected IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+}
