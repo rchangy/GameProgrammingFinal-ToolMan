b50c1a81ff383b9fa8e302395c843ded8dc1d2bb
許喬茵 <goodyhsu@xuqiaoyindeMacBook-Air.local>
2021-11-29 19:49:39 +0800

    player prefab Camera modified

--- ToolMan/Assets/Script/Mechanics/CameraManager.cs
+++ ToolMan/Assets/Script/Mechanics/CameraManager.cs
@@ -14,12 +14,12 @@ public class CameraManager : MonoBehaviour
 
     public void EnableFreeLook() {
         FreeLookCam.SetActive(true);
-        //MainCam.SetActive(false);
+        MainCam.SetActive(true);
     }
 
     public void EnableMain() {
         FreeLookCam.SetActive(false);
-        //MainCam.SetActive(true);
+        MainCam.SetActive(true);
         MainCam.transform.localPosition = new Vector3(0, 3, -10);
         MainCam.transform.localRotation = Quaternion.Euler(10, 0, 0);
     }
