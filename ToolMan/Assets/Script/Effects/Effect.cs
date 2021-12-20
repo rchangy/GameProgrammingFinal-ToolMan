49f3d6a63259708be10e7164d0e4b680e3bbae99
許喬茵 <goodyhsu@xuqiaoyindeMacBook-Air.local>
2021-12-20 20:03:18 +0800

    impact effect

--- ToolMan/Assets/Script/Effects/Effect.cs
+++ ToolMan/Assets/Script/Effects/Effect.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public abstract class Effect : MonoBehaviour
 {
     [SerializeField] protected GameObject effect;
-
+    
     Transform baseTransform;
     [SerializeField] Vector3 offset = Vector3.zero; // The effect will be played at (base transform + offset)
 
