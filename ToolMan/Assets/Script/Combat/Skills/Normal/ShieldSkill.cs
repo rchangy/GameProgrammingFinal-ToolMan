e3812a2979fd23792552d0bd33b8c6790fc7ad85
han-chihchang <han-chihchang@Han-Chihde-MBP.(none)>
2021-12-20 21:18:31 +0800

    shield skill delete buff

--- ToolMan/Assets/Script/Combat/Skills/Normal/ShieldSkill.cs
+++ ToolMan/Assets/Script/Combat/Skills/Normal/ShieldSkill.cs
@@ -7,15 +7,20 @@ namespace ToolMan.Combat.Skills.Normal
     [CreateAssetMenu(menuName = "ToolMan/Skill/PlayerSkill/Shield")]
     public class ShieldSkill : PlayerSkill
     {
-        public ScriptableBuff Buff;
+        //public ScriptableBuff Buff;
+        [SerializeField] private float _invulnerableTime;
+
 
         public override IEnumerator Attack(SkillCombat combat, BoolWrapper collisionEnable)
         {
             _toolController.AnimationAttack();
             yield return new WaitForSeconds(attackDelay);
-            _toolCombat.AddBuff(Buff);
-            _manCombat.AddBuff(Buff);
-            // delay
+            _toolCombat.Disable("Vulnerable");
+            _manCombat.Disable("Vulnerable");
+            yield return new WaitForSeconds(_invulnerableTime);
+            _toolCombat.RemoveDisable("Vulnerable");
+            _manCombat.RemoveDisable("Vulnerable");
+
         }
 
     }
