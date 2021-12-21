cad397c3ed290c6f433c0b073ec10e9474c907f9
lyanling <smile8www@gmail.com>
2021-12-21 22:59:46 +0800

    healthBar3D

--- ToolMan/Assets/Script/UI/HealthBar3D.cs
+++ ToolMan/Assets/Script/UI/HealthBar3D.cs
@@ -5,10 +5,12 @@ public class HealthBar3D : MonoBehaviour
     [SerializeField] private Transform _targetObject;
     [SerializeField] private Transform _healthBar;
     [SerializeField] private Camera _cam;
+    [SerializeField] private Transform cam;
     [SerializeField] private Vector3 _posOffset;
     private void LateUpdate()
     {
-        _healthBar.position = _cam.WorldToScreenPoint(_targetObject.position + _posOffset);
+        //_healthBar.position = _cam.WorldToScreenPoint(_targetObject.position + _posOffset);
         //Debug.Log(_targetObject.position);
+        transform.LookAt(transform.position + cam.forward);
     }
 }
