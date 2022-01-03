3fbd731bc94f1557b4ed0fb5518d6c398ba2636d
許喬茵 <goodyhsu@xuqiaoyindeMacBook-Air.local>
2022-01-03 15:21:50 +0800

    bad hint:(

--- ToolMan/Assets/import packages/UI/ScrollList/CircularScrollingList/_Demo/Scripts/IntListBox.cs
+++ ToolMan/Assets/import packages/UI/ScrollList/CircularScrollingList/_Demo/Scripts/IntListBox.cs
@@ -1,17 +1,24 @@
 ﻿using AirFishLab.ScrollingList;
 using UnityEngine;
 using UnityEngine.UI;
+using ToolMan.UI;
+using System.Collections.Generic;
 
 namespace AirFishLab.ScrollingList.Demo
 {
     public class IntListBox : ListBox
     {
+        //[SerializeField]
+        //private List<Hint> _hints;
+        private string[] _names = {"提示", "移動、跳躍", "變身", "十字鎬", "閃光彈", "轉視角", "水晶", "治療水晶", "抓取", "攻擊",
+        "記憶水晶", "光劍", "工具選擇", "鍵位——人型", "鍵位——工具", "龜龜", "迴力鏢", "大雞、小雞", "史萊姆兔", "護盾", "狗勾將軍", "族邸鯨魔閻",};
         [SerializeField]
         private Text _contentText;
 
         protected override void UpdateDisplayContent(object content)
         {
-            _contentText.text = ((int) content).ToString();
+            //_contentText.text = ((int) content).ToString();
+            _contentText.text = _names[(int)content-1];
         }
     }
 }
