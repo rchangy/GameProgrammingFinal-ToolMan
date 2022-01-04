1173bb08d4cbcb14b834c43f17c17a3ede91ece7
許喬茵 <goodyhsu@xuqiaoyindeMacBook-Air.local>
2022-01-04 22:22:37 +0800

    unlock all hints

--- ToolMan/Assets/Script/UI/HintPanel.cs
+++ ToolMan/Assets/Script/UI/HintPanel.cs
@@ -51,9 +51,10 @@ namespace ToolMan.UI
                 //Debug.Log("h = " + h._title + " b = " + b.name);
             }
 
-            CheckpointManager.LoadCheckpoint();
-            int lastUnlockedHint =  CheckpointManager.GetCheckpointInfo().lastUnlockedHint;
-            for (int i = 0; i < lastUnlockedHint; i++) _hints[i].Unlock();
+            //CheckpointManager.LoadCheckpoint();
+            //int lastUnlockedHint =  CheckpointManager.GetCheckpointInfo().lastUnlockedHint;
+            //for (int i = 0; i < lastUnlockedHint; i++) _hints[i].Unlock();
+            for (int i = 0; i < _hints.Count; i++) _hints[i].Unlock();
         }
 
         public void LoadHint(string hintTitle) {
