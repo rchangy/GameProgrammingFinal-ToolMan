b97cc632c45eaa4db7e38af65557a54b26100275
lyanling <smile8www@gmail.com>
2022-01-08 22:10:13 +0800

    checkpoint

--- ToolMan/Assets/Script/Mechanics/CheckpointManager.cs
+++ ToolMan/Assets/Script/Mechanics/CheckpointManager.cs
@@ -5,30 +5,23 @@ using UnityEngine;
 
 public static class CheckpointManager
 {
-    static Checkpoint ckpt;
-    static CheckpointInfo ckptInfo;
-    static string ckptPath = "Assets/Script/Mechanics/checkpoint.json";
+    //static Checkpoint ckpt;
+    static CheckpointInfo ckptInfo = null;
+    //static string ckptPath = "Assets/Script/Mechanics/checkpoint.json";
     static int[] player1ToolNums = { 1, 1, 2, 2, 2 };
     static int[] player2ToolNums = { 1, 2, 2, 3, 3 };
     static int[] lastUnlockedHIntList = { 1, 15, 17, 20, 21 };
-    static int current_level = -1;
     public static void LoadCheckpoint()
     {
-        if (!File.Exists(ckptPath) || current_level == -1)
+        if (ckptInfo == null)
         {
             UpdateCheckpoint(1);
         }
-        else if (current_level != -1)
-        {
-            Debug.Log("load checkpoint exist!");
-            //StreamReader r = new StreamReader(ckptPath);
-            //string jsonString = r.ReadToEnd();
-            //Debug.Log("jsonString " + jsonString);
-            //Debug.Log(jsonString);
-            //ckpt = JsonUtility.FromJson<Checkpoint>(jsonString);
-            ckptInfo = new CheckpointInfo(current_level);
-        }
-
+        //else
+        //{
+        //    Debug.Log("load checkpoint exist!");
+        //    ckptInfo = new CheckpointInfo(current_level);
+        //}
     }
 
     public static void NextLevel()
@@ -41,14 +34,13 @@ public static class CheckpointManager
     }
     public static void UpdateCheckpoint(int level)
     {
-        current_level = level;
         if (level <= 0 || level > 5)
         {
             Debug.Log("Error: Level is not in range of (1, 5).");
             return;
         }
         ckptInfo = new CheckpointInfo(level);
-        ckpt = ckptInfo.ToCheckpoint();
+        //ckpt = ckptInfo.ToCheckpoint();
         SaveCkeckpoint();
     }
 
@@ -76,10 +68,10 @@ public static class CheckpointManager
         public int player1ToolNum;
         public int player2ToolNum;
         public int lastUnlockedHint;
-        public CheckpointInfo(Checkpoint newCkpt)
-        {
-            setByLevel(int.Parse(newCkpt.level));
-        }
+        //public CheckpointInfo(Checkpoint newCkpt)
+        //{
+        //    setByLevel(int.Parse(newCkpt.level));
+        //}
         public CheckpointInfo(string level)
         {
             setByLevel(int.Parse(level));
