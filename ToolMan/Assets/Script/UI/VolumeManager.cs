e390e0866fd8d111136942b897d38c0999d1b46b
lyanling <smile8www@gmail.com>
2022-01-11 23:29:18 +0800

    a lot of bugs;))

--- ToolMan/Assets/Script/UI/VolumeManager.cs
+++ ToolMan/Assets/Script/UI/VolumeManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class VolumeManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
+    //    Volume volume = gameObject.GetComponent<Volume>();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 }
