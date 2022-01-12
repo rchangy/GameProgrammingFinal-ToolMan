08629a800f99ac8115370ab47010cb5a5a661857
han-chihchang <han-chihchang@Han-Chihde-MBP.(none)>
2022-01-12 22:03:32 +0800

    level 5 cut scene

--- ToolMan/Assets/Script/Player/PlayerController.StateAnimation.cs
+++ ToolMan/Assets/Script/Player/PlayerController.StateAnimation.cs
@@ -79,7 +79,35 @@ public partial class PlayerController
     }
     public void AnimtationForceToMan()
     {
-        animator.SetTrigger("toMan");
+        Effect toManEffect = effectController.effectList.Find(e => e.name == "ToManEffect");
+        toManEffect.PlayEffect();
         animator.SetBool("isTool", false);
+        animator.SetTrigger("toMan");
+    }
+    public void cutSceneToMan()
+    {
+        // reset parameters of animator
+        animator.SetBool("isTool", false);
+        //animator.SetTrigger("toMan");
+        animator.ResetTrigger("toTool");
+        animator.ResetTrigger("toPickaxe");
+        animator.ResetTrigger("toBoomerang");
+        animator.ResetTrigger("toShield");
+        animator.ResetTrigger("toFlashBomb");
+        animator.ResetTrigger("toLightSaber");
+        animator.ResetTrigger("Attack");
+
+        // reset grabbed point (need to fix this part if changing animation)
+        grabbedPoint.transform.localPosition = new Vector3(0.0f, -1.2f, 0.0f);
+        //point = new Vector3(0.0f, -1.2f, 0.0f);
+
+        // ==== reset player ==== //
+        gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+        transform.rotation = Quaternion.Euler(0, 154, 0);
+        // when a tool transform to a man, it may stuck into ground and fall down, this may need to be adjusted (position.y)
+        float newY;
+        newY = transform.position.y - GetCollider().bounds.extents.y - 1;
+        transform.position = new Vector3(gameObject.transform.position.x-1, newY, gameObject.transform.position.z + 0.5f);
+        //resetRigidBody();
     }
 }
